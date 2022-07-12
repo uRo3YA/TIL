e74cf19ff91289fe9cb8573e4c8aeb0838bd6647
uRo3YA <ltg8693@gmail.com>
2022-07-12 11:29:30 +0900

    파일정리

--- 2w/test1.py
+++ 2w/test1.py
@@ -1,7 +1,13 @@
-#실습1 
-#print("Hello\nWorld")
-#200304
-#a,b=map(float,input().split())
-a = input()
-n = int(a, 16)  
-print(format(n,'#o')[2:])
+n=0
+t=0
+#user_input=int(input())
+
+while True:
+    try:
+        user_input=int(input())
+        t+=1
+        n+=user_input
+        print("반복횟수:",t)
+        print("총합:",n)
+    except EOFError:
+        break
