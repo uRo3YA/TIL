e4a706f85370c4f69cf3d8aabd1a34c0abf07b8f
uRo3YA <ltg8693@gmail.com>
2022-07-13 17:23:37 +0900

    파일 정리

--- 2w/test1.py
+++ 2w/test1.py
@@ -1,3 +1,17 @@
-#9223372036854775807 9223372036854775808
-n,m=map(int,input().split())
-print(n+m)
+def prime(x):
+    if x ==1:            #1은 모든 수의 약수이기 때문에 pass
+        return False
+    for i in range(2,int(x**0.5)+1):  #제곱근이 있는 수 중에
+        if x%i==0:					#약수가 있으면 false	
+            return False
+    return True						#이외에는 소수
+
+while True:
+    n=int(input())
+    count=0
+    if n == 0 :						#0 입력하면 아웃되는 게 조건
+            break
+    for i in range(n,2*n+1):		#n과 2n+1사이에서
+        if prime(i):					#sosu함수안에 있는 게 있다면
+            count+=1					#카운트를 세라
+    print(count)		#개수를 출력하는 조건에 맞춰 카운트를 출력
