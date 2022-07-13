eb23e987f0110cc9c25835bb3d7a4e623c727288
uRo3YA <ltg8693@gmail.com>
2022-07-13 12:07:42 +0900

    파일 수정

--- 2w/test1.py
+++ 2w/test1.py
@@ -1,2 +1,8 @@
-#n=int(input())
-print(sum(map(int,input().split())))
+
+
+
+def my_func(**kwargs):
+    return kwargs
+r=my_func(name="홍",age=10,gender="M",etc=["20대","청주","비옴"])
+
+print(r,type(r))
