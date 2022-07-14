3232cef9a542dbf7e639cd946636a25f88e2ee8a
uRo3YA <ltg8693@gmail.com>
2022-07-14 14:11:53 +0900

    출력 수정

--- July/2w/test1.py
+++ July/2w/test1.py
@@ -4,13 +4,17 @@
 .upper(), .swapcase() 사용 금지
 banana #BANANA
 """
-word=list(input())
+word=list("banana")
 dic={'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
+dic2={'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
 for i in word:
     for j in dic:
             if i==j:
                 dic[j]+=1
-for k in dic:
-#for k in word:
+#for k in dic:
+for k in word:
     if dic[k]!=0:
-        print(k,dic[k])
+        if dic2[k]==0:
+            print(k,dic[k])
+        dic2[k]=1
+        
