2f420f31a6104cb1aa01e639da9e63107561b1ee
uRo3YA <ltg8693@gmail.com>
2022-07-16 20:19:19 +0900

    수정

--- memo.py
+++ memo.py
@@ -1,25 +1,3 @@
-import math
+n="abcdef"
+cnt=0
 
-def star(n):
-    s = []
-    if n==1:
-        s.append('***')
-        s.append('* *')
-        s.append('***')
-        
-    if n>=2:
-        t = star(n-1)
-        for i in range(len(t)):
-            s.append(t[i]*3)
-        for i in range(len(t)):
-            s.append(t[i] + ' '*(3**(n-1)) + t[i])
-        for i in range(len(t)):
-            s.append(t[i]*3)
-    return s
-
-num = int(input())
-n = int(math.log(num,3))
-
-s = star(n)
-for i in range(len(s)):
-    print(s[i])
