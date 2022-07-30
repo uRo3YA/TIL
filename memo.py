192cad2c52d8f00d9bfc0f358f603a370af571bd
uRo3YA <ltg8693@gmail.com>
2022-07-30 21:40:50 +0900

    0730

--- memo.py
+++ memo.py
@@ -1,10 +1,19 @@
-sum_list=[]
-for i in range(5):
-    a,b,c,d=map(int,input().split())   
-    sum_list.append(a+b+c+d)
-print(sum_list)
-tmp=max(sum_list)
-print(tmp)
-index = sum_list.index(tmp)
-print(index)    
-print(index+1, max(sum_list))
+from math import sqrt
+
+
+n=int(input())
+#m=sqrt(n)
+m=(int(sqrt(n)))
+m_list=[]
+
+## 11 9
+while True:
+    if n==0:
+        break
+    if n>=(m**2):
+        m_list.append(m) 
+        n=n-(m**2) 
+    else:
+        m-=1
+print(len(m_list))
+print((m_list))
