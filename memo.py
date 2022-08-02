578443c8a6a44f5276f01bc98a213c4dec1222dc
uRo3YA <ltg8693@gmail.com>
2022-08-02 14:22:07 +0900

    0802

--- memo.py
+++ memo.py
@@ -1,5 +1,16 @@
-# 1
-# 3
-# A 10
-# B 7
-# C 5 
+import sys
+import heapq
+input = sys.stdin.readline
+
+min_heap = []
+
+for _ in range(int(input())):
+    n = int(input())
+    
+    if n == 0:
+        if len(min_heap):
+            print(-1*(heapq.heappop(min_heap)))
+        else:
+            print(0)
+    else:
+        heapq.heappush(min_heap, -1*(n))
