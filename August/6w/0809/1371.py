65f62449174911bacbb50cfc41ef3e5d604c4cc5
uRo3YA <ltg8693@gmail.com>
2022-08-09 15:53:40 +0900

    0809

--- August/6w/0809/1371.py
+++ August/6w/0809/1371.py
@@ -1,10 +1,21 @@
-import sys
+dic = {}
 
-s = sys.stdin.read()
-li = [0]*26
-for c in s:
-    if c.islower():
-        li[ord(c)-97] += 1
-for i in range(26):
-    if li[i] == max(li):
-        print(chr(97+i), end='')
+while True:
+    try:
+        for c in input():
+            if c != ' ':
+                dic[c] = dic.get(c, 0) + 1 
+    except EOFError:
+        arr = []
+        for c, num in dic.items():
+            arr.append([c, num])
+        arr.sort(key=lambda x: (-x[1], x[0]))
+        max_val = arr[0][1]
+        result = ''
+        for c, num in arr:
+            if num == max_val:
+                result += c
+            else:
+                break
+        print(result)
+        break
