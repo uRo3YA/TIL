9eb4a508a362346fd77b31707ab1372c26778b25
uRo3YA <ltg8693@gmail.com>
2022-08-16 14:38:52 +0900

    0816 SQL 실습

---
+++ SQL/tutorial/05_user.sql
@@ -0,0 +1,15 @@
+
+
+CREATE TABLE users (
+
+last_name Text NOT NULL,
+first_name Text NOT NULL,
+age INTEGER NOT NULL,
+country Text NOT NULL,
+phone INTEGER NOT NULL,
+
+);
+
+-- csv import 하기
+.mode csv 
+.import users.csv users
