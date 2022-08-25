d1061292f86348458525be43d12d640d642e13a9
uRo3YA <ltg8693@gmail.com>
2022-08-25 15:39:02 +0900

    0825

--- August/8w/0824/DB-ORM-master/db/models.py
+++ August/8w/0824/DB-ORM-master/db/models.py
@@ -18,7 +18,18 @@ class Director(models.Model):
 class Genre(models.Model):
     title = models.TextField()
 
+class Director(models.Model):
+    name = models.TextField()
+    debut = models.DateTimeField()
+    country = models.TextField()
 
+class Movie(models.Model):
+    director = models.ForeignKey(Director,on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre,on_delete=models.CASCADE)
+    title = models.TextField()
+    opening_date = models.DateField()
+    running_time = models.IntegerField()
+    screening = models.BooleanField()
 # class Person:
 #     pass 
 
