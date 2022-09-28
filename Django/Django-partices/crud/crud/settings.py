b3596a70c5c0afde019eb08d4153ef6273f2eeb2
uRo3YA <ltg8693@gmail.com>
2022-09-29 03:50:58 +0900

    베이스 분리가 안됨

--- Django/Django-partices/crud/crud/settings.py
+++ Django/Django-partices/crud/crud/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "crud.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR, "templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -104,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
