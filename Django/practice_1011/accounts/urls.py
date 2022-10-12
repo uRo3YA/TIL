0eff4ecd3bba7af1207caeebac098515c7f11651
uRo3YA <ltg8693@gmail.com>
2022-10-12 15:35:38 +0900

    로그인,로그아웃,네비바

--- Django/practice_1011/accounts/urls.py
+++ Django/practice_1011/accounts/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     # path('accounts/',views.index, name="index"),
     path("signup/", views.signup, name="signup"),
     path("<int:pk>/", views.detail, name="detail"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
--- Django/practice_1011/accounts/views.py
+++ Django/practice_1011/accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 
-
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
 from .models import User
-
-# from .models import User
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
 
 # Create your views here.
@@ -32,3 +33,26 @@ def detail(request, pk):
     user = get_user_model().objects.get(pk=pk)
     context = {"user": user}
     return render(request, "accounts/detail.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # 세션에 저장
+            # login 함수는 request, user 객체를 인자로 받음
+            # user 객체는 어디있어요? 바로 form에서 인증된 유저 정보를 받을 수 있음
+            auth_login(request, form.get_user())
+            # http://127.0.0.1:8000/accounts/login/?next=/articles/1/update/
+            # request.GET.get('next') : /articles/1/update/
+            return redirect(request.GET.get("next") or "articles:index")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("articles:index")
