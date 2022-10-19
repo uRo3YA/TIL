27835253927f8c39c6bc7f28314b56874a135ce6
uRo3YA <ltg8693@gmail.com>
2022-10-19 15:21:44 +0900

    Comment : view/template

--- Django/Django-modelfom/articles/views.py
+++ Django/Django-modelfom/articles/views.py
@@ -79,12 +79,14 @@ def update(request, pk):
             return redirect("articles:detail", article.pk)
 
 
+@login_required
 def comment_create(request, pk):
     article = Article.objects.get(pk=pk)
     comment_form = CommentForm(request.POST)
     if comment_form.is_valid():
         comment = comment_form.save(commit=False)
         comment.article = article
+        comment.user = request.user
         comment.save()
     return redirect("articles:detail", article.pk)
 
