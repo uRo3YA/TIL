176faddceeda3c5a25026578d7d5695a02872df7
uRo3YA <ltg8693@gmail.com>
2022-10-19 15:19:07 +0900

    Comment : model

--- Django/Django-modelfom/articles/models.py
+++ Django/Django-modelfom/articles/models.py
@@ -30,3 +30,4 @@ class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- Django/Django-modelfom/articles/views.py
+++ Django/Django-modelfom/articles/views.py
@@ -59,20 +59,24 @@ def detail(request, pk):
 @login_required
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    if request.method == "POST":
-        # POST : input 값 가져와서, 검증하고, DB에 저장
-        article_form = ArticleForm(request.POST, request.FILES, instance=article)
-        if article_form.is_valid():
-            # 유효성 검사 통과하면 저장하고, 상세보기 페이지로
-            article_form.save()
-            messages.success(request, "글이 수정되었습니다.")
+    if request.user == article.user:
+        if request.method == "POST":
+            # POST : input 값 가져와서, 검증하고, DB에 저장
+            article_form = ArticleForm(request.POST, request.FILES, instance=article)
+            if article_form.is_valid():
+                # 유효성 검사 통과하면 저장하고, 상세보기 페이지로
+                article_form.save()
+                messages.success(request, "글이 수정되었습니다.")
+                return redirect("articles:detail", article.pk)
+            # 유효성 검사 통과하지 않으면 => context 부터해서 오류메시지 담긴 article_form을 랜더링
+        else:
+            # 작성자가 아닐 때
+            # (1) 403 에러메시지를 던져버린다.
+            # from django.http import HttpResponseForbidden
+            # return HttpResponseForbidden()
+            # (2) flash message 활용!
+            messages.warning(request, "작성자만 수정할 수 있습니다.")
             return redirect("articles:detail", article.pk)
-        # 유효성 검사 통과하지 않으면 => context 부터해서 오류메시지 담긴 article_form을 랜더링
-    else:
-        # GET : Form을 제공
-        article_form = ArticleForm(instance=article)
-    context = {"article_form": article_form}
-    return render(request, "articles/form.html", context)
 
 
 def comment_create(request, pk):
