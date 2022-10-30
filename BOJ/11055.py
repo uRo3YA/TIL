a8e69e8459832ffff869775b3b0a0c05b6f2efef
uRo3YA <ltg8693@gmail.com>
2022-10-30 22:11:37 +0900

    1030

---
+++ BOJ/11055.py
@@ -0,0 +1,10 @@
+
+n =int(input())
+a = list(map(int,input().split()))
+dp = a[:]
+
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], dp[j] + a[i])
+print(max(dp))
