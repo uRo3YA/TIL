638f160779daa175e4baa1b85653996899d35a2c
LeeTaeGeuk <108652200+uRo3YA@users.noreply.github.com>
2023-02-02 21:02:34 +0900

    [Silver II] Title: 친구, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -13,8 +13,11 @@ for i in range(n):
 		for k in range(n):
 			if j==k:
 				continue
+			# 어떤 사람 A가 또다른 사람 B의 2-친구가 되기 위해선, 두 사람이 친구이거나,  A와 친구이고, B와 친구인 C가 존재해야 된다.
 			if matrix[j][k]=="Y" or (matrix[j][i]=="Y" and matrix[i][k]=="Y"):
 				visit[j][k]=True
+
+#결과값
 result=0
 for i in visit:
 	result=max(result,sum(i))
