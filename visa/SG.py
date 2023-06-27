607e96cc23a7ae416c152f1e55dbba88069100a8
uRo3YA <ltg8693@gmail.com>
2023-06-27 20:53:55 +0900

    sg

---
+++ visa/SG.py
@@ -0,0 +1,23 @@
+import pyvisa as visa
+import time
+ 
+# initialize visa resource
+rm = visa.ResourceManager()
+ 
+# open the resource using the VISA address from Keysight Connection Expert
+SGC = rm.open_resource('TCPIP::XXX.XXX.XXX.XXX::INSTR') 
+ 
+# request the instrument to identify itself
+modelSerialnumber = SGC.query('*IDN?')
+
+SGC.write("SOUR:FREQ 3.2 GHz")
+SGC.write("SOUR:POW:OFFS 1 dBm")
+SGC.write("SOUR:POW -1.3 dBm")
+SGC.write("OUTP ON")
+print(modelSerialnumber)
+print(SGC.query("SOUR:FREQ?"))
+print(SGC.query("SOUR:POW?"))
+print(SGC.query("OUTP?"))
+# close
+SGC.close()
+rm.close()
