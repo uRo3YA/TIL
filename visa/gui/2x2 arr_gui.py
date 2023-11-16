d06ab280478b4a89adc4cdb3cfba86135eba4f89
uRo3YA <ltg8693@gmail.com>
2023-11-16 22:56:22 +0900

    2x2 수정

--- visa/gui/2x2 arr_gui.py
+++ visa/gui/2x2 arr_gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QRadioButton, QPushButton, QButtonGroup, QLineEdit, QGroupBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QRadioButton, QPushButton, QButtonGroup, QLineEdit, QGroupBox,QLabel
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -18,7 +18,7 @@ class MyWindow(QWidget):
         radio_button2 = QRadioButton('Radio 2')
 
         # 라디오 버튼들을 그룹으로 묶어 서로 토글되게 함
-        radio_group = QButtonGroup()
+        radio_group = QButtonGroup(self)
         radio_group.addButton(radio_button1)
         radio_group.addButton(radio_button2)
 
@@ -26,15 +26,14 @@ class MyWindow(QWidget):
         group1_layout.addWidget(radio_button2)
 
         # Connect 푸시 버튼 추가
-        text_input1 = QLineEdit()
-        connect_button = QPushButton('Connect')
+        connect_button = QPushButton('Connect',self)
         connect_button.clicked.connect(self.on_connect_button_click)
+        text_input1 = QLineEdit()
         
 
         group1_layout.addWidget(text_input1)
         group1_layout.addWidget(connect_button)
         
-
 
         # 두 번째 그룹 레이아웃
         group2_box = QGroupBox('Group 2')
@@ -44,45 +43,55 @@ class MyWindow(QWidget):
         button5 = QPushButton('Button 5')
 
         group2_layout.addWidget(button3)
-        group2_layout.addWidget(button4)
-        group2_layout.addWidget(button5)
-
-        # 그룹1_layout과 그룹2_layout을 수평으로 결합
-        group1_and_group2_layout = QHBoxLayout()
-        group1_and_group2_layout.addWidget(group1_box)
-        group1_and_group2_layout.addWidget(group2_box)
-
+        # group2_layout.addWidget(button4)
+        # group2_layout.addWidget(button5)
+        perv_and_next_button_layout = QHBoxLayout()
+        perv_and_next_button_layout.addWidget(button4)
+        perv_and_next_button_layout.addWidget(button5)
+        group2_layout.addLayout(perv_and_next_button_layout)
         # 세 번째 그룹 레이아웃
         group3_box = QGroupBox('Group 3')
         group3_layout = QVBoxLayout(group3_box)  # 수정된 부분
+        feq_label=QLabel("주파수 : ")
         text_input3 = QLineEdit()
         text_input3.setFixedWidth(100)  # text_input3의 폭을 100으로 설정
-        button6 = QPushButton('Button 6')
-        button6.setFixedWidth(100)  # button6의 폭을 100으로 설정
+        button6 = QPushButton('set')
+        button6.setFixedWidth(40)  # button6의 폭을 100으로 설정
 
         # 텍스트 입력과 버튼을 양쪽 끝으로 정렬하는 수평 레이아웃
         text_input_and_button_layout = QHBoxLayout()
+        text_input_and_button_layout.addWidget(feq_label)
         text_input_and_button_layout.addWidget(text_input3)
-        text_input_and_button_layout.addStretch(1)  # 왼쪽에 빈 공간 추가
+        text_input_and_button_layout.addStretch(2)  # 왼쪽에 빈 공간 추가
         text_input_and_button_layout.addWidget(button6)
-        
+
 
         group3_layout.addLayout(text_input_and_button_layout)
 
         # 버튼7과 텍스트 입력7을 수평으로 배치
+        amp_label=QLabel("Amp   :")
         text_input5 = QLineEdit()
-        button7 = QPushButton('Button 7')
+        text_input5.setFixedWidth(100)
+        button7 = QPushButton('set')
+        button7.setFixedWidth(40)
         group3_and_button7_layout = QHBoxLayout()
+        group3_and_button7_layout.addWidget(amp_label)
         group3_and_button7_layout.addWidget(text_input5)
+        group3_and_button7_layout.addStretch(2)
         group3_and_button7_layout.addWidget(button7)
 
         group3_layout.addLayout(group3_and_button7_layout)
 
         # 버튼8과 텍스트 입력8을 수평으로 배치
+        offset_label=QLabel("Offset : ")
         text_input6 = QLineEdit()
-        button8 = QPushButton('Button 8')
+        text_input6.setFixedWidth(100)
+        button8 = QPushButton('set')
+        button8.setFixedWidth(40)
         group3_and_button8_layout = QHBoxLayout()
+        group3_and_button8_layout.addWidget(offset_label)
         group3_and_button8_layout.addWidget(text_input6)
+        group3_and_button8_layout.addStretch(2)
         group3_and_button8_layout.addWidget(button8)
 
         group3_layout.addLayout(group3_and_button8_layout)
@@ -100,8 +109,13 @@ class MyWindow(QWidget):
 
         # 그룹3_layout과 그룹4_layout을 수평으로 결합
         group3_and_group4_layout = QHBoxLayout()
-        group3_and_group4_layout.addWidget(group3_box)
-        group3_and_group4_layout.addWidget(group4_box)
+        group3_and_group4_layout.addWidget(group3_box, stretch=1)  # group3_box가 늘어날 수 있도록 stretch 추가
+        group3_and_group4_layout.addWidget(group4_box, stretch=1)  # group4_box가 늘어날 수 있도록 stretch 추가
+
+        # 그룹1_box와 그룹2_box를 수평으로 결합
+        group1_and_group2_layout = QHBoxLayout()
+        group1_and_group2_layout.addWidget(group1_box, stretch=1)  # group1_box가 늘어날 수 있도록 stretch 추가
+        group1_and_group2_layout.addWidget(group2_box, stretch=1)  # group2_box가 늘어날 수 있도록 stretch 추가
 
         # 그룹 레이아웃들을 전체 레이아웃에 추가
         main_layout.addLayout(group1_and_group2_layout)
@@ -114,8 +128,10 @@ class MyWindow(QWidget):
         self.show()
 
     def on_connect_button_click(self):
-        print("Connect button clicked!")
-
+        for radio_button in self.findChildren(QRadioButton):
+            if radio_button.isChecked():
+                print(f'{radio_button.text()}이 선택되었습니다.')
+                   
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyWindow()
